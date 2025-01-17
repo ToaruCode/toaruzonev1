dd6a3518a86fea84c1e91ef7181d55e83b741f2f
ToaruCode <toaru2024yt@gmail.com>
2025-01-17 17:33:15 +1100

    menu

---
+++ lib/constants.ts
@@ -0,0 +1,6 @@
+export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || 'ToaruZone'
+export const APP_VERSION = process.env.NEXT_PUBLIC_APP_VERSION || '0.0.1'
+export const APP_DESCRIPTION =
+  process.env.NEXT_PUBLIC_APP_DESCRIPTION || 'A small site belongs to toaru.'
+export const APP_SLOGAN =
+  process.env.NEXT_PUBLIC_APP_SLOGAN || 'Learn, Share, Grow.'
